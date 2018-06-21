4ed0f5921779d01192f862d0bc85c2eb17c99a63
vishwa8545 <vishwa8313@gmail.com>
2018-06-21 20:12:05 +0530

    added updating feature to mini-amazon

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, send_from_directory,jsonify
 
 app = Flask('Amazon')
+
+
 products = []
 @app.route('/home', methods=['GET' , 'POST'])
 def product():
     if request.method == 'GET':
-        query = request.args['name']
+        query = request.args['product_name']
         for  prob in products:
             if prob['name'] == query:
                 return jsonify(prob)
@@ -14,16 +16,25 @@ def product():
 
 
     elif request.method == 'POST':
-        name = request.form['product_name']
-        dsc = request.form['product_disc']
-        price = request.form['product_price']
-        prob = {
-            'name':name,
-            'desc':dsc,
-            'price':price
-        }
-        products.append(prob)
-        return send_from_directory('static','index.html')
+        if request.form['op_type'] == 'insert':
+            name = request.form['product_name']
+            dsc = request.form['product_disc']
+            price = request.form['product_price']
+            prob = {
+                'name':name,
+                'desc':dsc,
+                'price':price
+            }
+            products.append(prob)
+            return send_from_directory('static','index.html')
+        if request.form['op_type'] == 'update':
+            for product in products:
+                if request.form['name'] == product['name']:
+                    product['desc'] = request.form['disc']
+                    product['price'] = request.form['price']
+                    return jsonify(product)
+            return "not updated"
+
 
 @app.route('/' , methods=['GET'])
 def index():
@@ -31,5 +42,5 @@ def index():
 
 
 if __name__ == '__main__':
-     app.run(host='127.0.0.1',port=5000)
+     app.run(host='0.0.0.0',port=5000)
 
